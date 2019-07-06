a3f32529573abacb03a12c84a8b93fd211a49b59
AveryanovS <averyanov@my.com>
2019-07-06 12:15:07 +0300

    commit_of_06-07-2019

---
+++ commit.js
@@ -0,0 +1,17 @@
+const { repo, commits, date } = require('./config.json');
+const exec = require('child_process').exec;
+const moment = require('moment');
+const fs = require('fs');
+console.log(moment(date).diff(moment(), 'days'));
+exec(`cd repo; git init; git remote add origin ${repo}`,
+    () => {
+        fs.appendFile('repo/file.txt', String( new Date() ), error => {
+            if(error)
+                return console.error(error);
+            exec(`git add -A; git commit -m "commit_of_${moment().format('DD-MM-YYYY')}"; git push;`,
+                (_error, stdout, stderr) => {
+                    console.log(stdout);
+                    console.error(stderr);
+                });
+        });
+    });
