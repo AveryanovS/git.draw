d5d106daa8930ecd9c8fb05e2ae9b5d6116b7b8a
AveryanovS <averyanov@my.com>
2019-07-08 18:54:22 +0300

    Fix

--- commit.js
+++ commit.js
@@ -4,13 +4,33 @@ const moment = require('moment');
 const fs = require('fs');
 const { scheduleJob } = require('node-schedule');
 
+const repoParts = ( repo.split('/').splice(-1) )[0].split('.');
+repoParts.splice(-1);
+const repoName = repoParts.join('.');
+
+fs.exists(`${__dirname}/${repoName}`, exists => {
+    if(!exists)
+        return exec(`cd ${__dirname}; git clone ${repo}`,
+            (error, stdout, stderr) => {
+                console.log(stdout);
+                console.error(stderr);
+                if(error)
+                    return console.error(error);
+                fs.appendFile(`${__dirname}/${repoName}/file.txt`, String(new Date()), error => {
+                    if(error)
+                        console.error(error);
+                    console.log('repo ready');
+                });
+            });
+    console.log('repo ready');
+});
 
 const commit = (count = 1) => {
-    fs.appendFile(`${__dirname}/repo/file.txt`, String( new Date() ), error => {
+    fs.appendFile(`${__dirname}/${repoName}/file.txt`, String( new Date() ), error => {
         if(error)
             return console.error(error);
         console.log(new Date(), `commiting, ${count - 1} left`);
-        exec(`cd ${__dirname}/repo; git add -A; git commit -m "commit_of_${moment().format('DD-MM-YYYY')}"; git push --set-upstream origin master;`,
+        exec(`cd ${__dirname}/${repoName}; git add -A; git commit -m "commit_of_${moment().format('DD-MM-YYYY')}"; git push --set-upstream origin master;`,
             (_error, stdout, stderr) => {
                 console.log(stdout);
                 console.error(stderr);
@@ -25,8 +45,8 @@ const dailyJob = () => {
     if(commitsIndex < 0)
         console.log(new Date(), 'too early', commitsIndex);
     else
-        exec(`cd ${__dirname}/repo; git init; git remote add origin ${repo}`,
-            (_err, stdout, stderr) => {
+        exec(`cd ${__dirname}/${repoName}; git init; git remote add origin ${repo}`,
+            (_error, stdout, stderr) => {
                 console.log(stdout);
                 console.error(stderr);
                 commit(commits[commitsIndex]);
